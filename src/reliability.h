21a121afa64049568a3a70063704c7e83e0ea143
wyatt lavigueure <wyatt.l@digipen.edu>
2017-05-13 20:47:36 -0700

    started reliability layer

---
+++ src/reliability.h
@@ -0,0 +1,12 @@
+/*
+  \author
+    Wyatt Lavigueure
+  \date
+    5/13/2017
+  \brief
+    Reliablity layer of the network stack. If a packet is marked as reliable then it will be resent.
+*/
+#ifndef RELI_H
+#define RELI_H
+
+#endif

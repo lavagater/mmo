a4c7a087d315b0ddeec24c658fe373335c94f645
wyatt lavigueure <wyatt.l@digipen.edu>
2017-06-21 14:13:32 -0700

    created a simple database, and did a little testing, unit test still needed

--- src/database.cpp
+++ src/database.cpp
@@ -1,13 +1,97 @@
-/*****************************************************************************/
-/*!
-\author Wyatt Lavigueure
-\date   5/2/2017
-\brief  
-  The entry point for the database executable
 
-*/
-/*****************************************************************************/
-int main()
+
+#include "database.h"
+
+Database::Database(char *table_name) : file(table_name, std::ios_base::binary | std::ios_base::in | std::ios_base::out), size(0), rows()
 {
-  return 420;
+  //set the rows
+  unsigned num_rows;
+  file.read(reinterpret_cast<char*>(&num_rows), sizeof(unsigned));
+  for (unsigned i = 0; i < num_rows; ++i)
+  {
+    unsigned row_size;
+    file.read(reinterpret_cast<char*>(&row_size), sizeof(unsigned));
+    rows.push_back(row_size);
+  }
+  //set the size
+  file.read(reinterpret_cast<char*>(&size), sizeof(unsigned));
+  //save the object size
+  for (unsigned i = 0; i < rows.size(); ++i)
+  {
+    object_size += rows[i];
+  }
+}
+
+void Database::UpdateSize()
+{
+  //seek file to where the size goes
+  file.seekg((1+rows.size()) * sizeof(unsigned));
+  file.write(reinterpret_cast<char*>(&size), sizeof(unsigned));
+}
+
+Database::Database(char *table_name, std::vector<unsigned> rows) :file(table_name, std::ios_base::binary | std::ios_base::in | std::ios_base::out | std::ios_base::trunc), size(0), rows(rows)
+{
+  //write the rows to the file
+  unsigned num_rows = rows.size();
+  file.write(reinterpret_cast<char*>(&num_rows), sizeof(unsigned));
+  for (unsigned i = 0; i < num_rows; ++i)
+  {
+    unsigned row_size = rows[i];
+    file.write(reinterpret_cast<char*>(&row_size), sizeof(unsigned));
+  }
+  //write the size
+  UpdateSize();
+  //save the object size
+  for (unsigned i = 0; i < rows.size(); ++i)
+  {
+    object_size += rows[i];
+  }
+}
+
+char *Database::Get(unsigned id)
+{
+  //allocate memory to return
+  char *res = new char[object_size];
+  //go to the spot in the file, (2 + rows.size()) * sizeof(unsigned) is the size of the header
+  file.seekg((2 + rows.size()) * sizeof(unsigned) + id * object_size);
+  //read the entire object
+  file.read(res, object_size);
+  return res;
+}
+
+char *Database::Get(unsigned id, unsigned row)
+{
+  //find out how far into the object to get to the row we want
+  unsigned split_size = 0;
+  for (unsigned i = 0; i < row; ++i)
+  {
+    split_size += rows[i];
+  }
+  //allocate memory to return
+  char *res = new char[rows[row]];
+  //go to the spot in the file, (2 + rows.size()) * sizeof(unsigned) is the size of the header
+  file.seekg((2 + rows.size()) * sizeof(unsigned) + id * object_size + split_size);
+  //read just the specified row
+  file.read(res, rows[row]);
+  return res;
+}
+
+void Database::Set(unsigned id, unsigned row, const void *data)
+{
+  //find out how far into the object to get to the row we want
+  unsigned split_size = 0;
+  for (unsigned i = 0; i < row; ++i)
+  {
+    split_size += rows[i];
+  }
+  //go to the spot in the file, (2 + rows.size()) * sizeof(unsigned) is the size of the header
+  file.seekg((2 + rows.size()) * sizeof(unsigned) + id * object_size + split_size);
+  //write the data
+  file.write(reinterpret_cast<const char*>(data), rows[row]);
+  //if we are edting an object that is past size we need to update size
+  if (size < id)
+  {
+    size = id;
+    UpdateSize();
+  }
 }
---
+++ src/database.h
@@ -0,0 +1,86 @@
+/*!
+  \brief
+    Each table is a seperate file, each table has a header which describes what data is in the table, 
+    the header is first thing is an unsigned which represents the number of fields/rows 
+    then for each row there is an unsigned representing the size in bytes that the row element is.
+    then there is an unsigned which represents the number or objects/column.
+
+    The header size is 2 * sizeof(unsigned) + num_rows * sizeof(unsigned)
+    and example header could look like
+    3 4 4 8 74
+    which would mean there are 3 rows first row is 4 bytes second row is 4 bytes and third row is 8 bytes and there is 74 columns
+    the files are stored in bianary.
+  \author
+    Wyatt Lavigueure
+  \date
+    6/20/2017
+*/
+#include <vector>
+#include <fstream>
+/*!
+  \brief
+    Each databse class just handles one table, this is to try and increase scaling of the database
+*/
+class Database
+{
+public:
+  /*!
+    \brief
+      Opens the file for table_name table and loads in the data, if the table is unable to be opened nothing happens
+    \param table_name
+      The name of the table for the database to open
+  */
+  Database(char *table_name);
+  /*!
+    \brief
+      Creates a new table called table_name with rows given by rows,if there was already a table with the given name it is replaced
+      and all previous data is lost
+    \param table_name
+      The name of the table for the database to open
+    \param rows
+      the size in bytes of each row
+  */
+  Database(char *table_name, std::vector<unsigned> rows);
+  /*!
+    \brief
+      Gets object/column corisponding to id from the table, returns the data in a tightly packet format(no pad bytes)
+    \param id
+      which object/column in the database to get data from
+    \return
+      pointer to the data, the memory is created by calling new char[], and should be deleted with delete [].
+  */
+  char *Get(unsigned id);
+  /*!
+    \brief
+      Gets a speciic row from an object/column from the table, returns the data in a tightly packet format(no pad bytes)
+    \param id
+      which object/column in the database to get data from
+    \param row
+      the row in the table
+    \return
+      pointer to the data, the memory is created by calling new char[], and should be deleted with delete [].
+  */
+  char *Get(unsigned id, unsigned row);
+  /*!
+    \brief
+      Sets the value of a row for a specific column
+    \param id
+      which object/column in the database
+    \param row
+      the row in the table
+    \param data
+      raw data to copy into the database
+  */
+  void Set(unsigned id, unsigned row, const void *data);
+private:
+  //the file on harddrive
+  std::fstream file;
+  //number of objects in file
+  unsigned size;
+  //the rows
+  std::vector<unsigned> rows;
+  //the size of the object(sum of every entry in rows)
+  unsigned object_size;
+  //writes the size to the file
+  void UpdateSize();
+};

94a8e801859819144f3d0fc451a3c35bdfc1bb66
wyatt lavigueure <wyatt.l@digipen.edu>
2018-06-17 11:47:04 -0700

    Added a dll interface to the network stak for the client

---
+++ network_dll/network_dll/network_stack_dll.h
@@ -0,0 +1,40 @@
+#ifndef NETWORK_DLL_H
+#define NETWORK_DLL_H
+
+#ifdef NETWORK_DLL_EXPORT
+#define NETWORK_DLL_API __declspec(dllexport) 
+#else
+#define NETWORK_DLL_API __declspec(dllimport) 
+#endif
+
+#include <vector>
+#include "network_stack.h"
+
+//contains the things that the client wants to know about
+class session
+{
+public:
+  //the flags for sending messages
+  BitArray<HEADERSIZE> flags;
+  //buffer for receiving messages
+  char buffer[MAXPACKETSIZE];
+  sockaddr_in local;
+  sockaddr_in server;
+  SOCKET sock;
+  NetworkStack *stack;
+};
+
+extern std::vector<session> dll_sessions;
+extern std::vector<int> dll_unused_ids;
+
+
+extern "C" {
+  NETWORK_DLL_API int CreateSession(char *ip, int port);
+  NETWORK_DLL_API void DeleteSession(int id);
+  NETWORK_DLL_API int SessionSend(int id, char *data, int length);
+  NETWORK_DLL_API int SessionRecieve(int id, char *data, int length);
+  NETWORK_DLL_API void EnableEncryption(int id);
+}
+
+
+#endif //header guard
